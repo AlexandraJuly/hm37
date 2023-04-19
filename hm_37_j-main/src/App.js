f5e3eeccad4fd45a673ddc0e58d5c515a6bd3012
AlexandraJuly <catmon0201@gmail.com>
2023-04-19 14:54:28 +0300

    1

---
+++ hm_37_j-main/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import MainLayout from './MainLayout/MainLayout';
+
+function App() {
+  return (
+  <>
+  <h1>
+    Front End Pro
+    Homework №37
+  </h1>
+  <h2>
+    Student: Dima Kharlamov
+  </h2>
+   <MainLayout />;
+  </>
+);
+}
+
+export default App;
